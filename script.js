cbccf133e45de6f45c4996c336c157eea31fa63c
Prathamesh0816 <93310546+Prathamesh0816@users.noreply.github.com>
2024-08-26 21:06:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function addTwoNumbers(){
+    const num1 = parseInt(prompt("Enter number 1 : "));
+    const num2 = parseInt(prompt("Enter number 2 : "));
+    add = num1 + num2;
+    alert("Addition is : "+add);
+}
+function subtractTwoNumbers(){
+    const num1 = parseInt(prompt("Enter number 1 : "));
+    const num2 = parseInt(prompt("Enter number 2 : "));
+    subtract = num1 - num2;
+    alert("Subtraction is : "+subtract);
+}
+function multiplyTwoNumbers(){
+    const num1 = parseInt(prompt("Enter number 1 : "));
+    const num2 = parseInt(prompt("Enter number 2 : "));
+    multiply = num1 * num2;
+    alert("Multiplication is : "+multiply);
+}
+function divideTwoNumbers(){
+    const num1 = parseInt(prompt("Enter number 1 : "));
+    const num2 = parseInt(prompt("Enter number 2 : "));
+    divide = num1 / num2;
+    alert("Division is : "+divide);
+}
+function modulusTwoNumbers(){
+    const num1 = parseInt(prompt("Enter number 1 : "));
+    const num2 = parseInt(prompt("Enter number 2 : "));
+    mod = num1 % num2;
+    alert("Modulus is : "+mod);
+}
